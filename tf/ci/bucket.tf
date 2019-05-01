e9624ff93665bfbfd9a6c3a9b7e16846765953a2
Daniel Jones <daniel.jones@engineerbetter.com>
2019-05-01 14:50:51 +0100

    new project id and link billing

--- tf/ci/bucket.tf
+++ tf/ci/bucket.tf
@@ -8,15 +8,21 @@ data "google_organization" "org" {
   domain = "engineerbetter.com"
 }
 
+data "google_billing_account" "acct" {
+  display_name = "EngineerBetter"
+  open         = true
+}
+
 data "google_active_folder" "spikes" {
   display_name = "spikes"
   parent       = "organizations/${data.google_organization.org.id}"
 }
 
 resource "google_project" "my_project" {
-  name       = "GCP Spike"
-  project_id = "dj190501-a"
-  folder_id  = "${data.google_active_folder.spikes.name}"
+  name            = "GCP Spike"
+  project_id      = "dj190501-b"
+  folder_id       = "${data.google_active_folder.spikes.name}"
+  billing_account = "${data.google_billing_account.acct.id}"
 }
 
 resource "google_storage_bucket" "ci" {

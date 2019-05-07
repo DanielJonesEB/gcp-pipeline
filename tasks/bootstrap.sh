83f0f32e5ef83e7c0105e8b3926b3824faf4d245
Daniel Jones <daniel.jones@engineerbetter.com>
2019-05-07 10:10:15 +0100

    parameterise all the things

--- tasks/bootstrap.sh
+++ tasks/bootstrap.sh
@@ -2,10 +2,15 @@
 
 set -euxo pipefail
 
-: "${GCP_CREDS_JSON:?GCP_CREDS_JSON env var must provide contents of credentials file}"
+: "${BILLING_ACCOUNT_ID:?BILLING_ACCOUNT_ID env var must be provided}"
+: "${BUCKET_LOCATION:?BUCKET_LOCATION env var must provide region of bucket to hold Terraform state (eg 'EU')}"
+: "${FOLDER_NAME:?FOLDER_NAME env var must provide name of extant folder into which new project will be created}"
+: "${GCP_CREDENTIALS_JSON:?GCP_CREDENTIALS_JSON env var must provide contents of credentials file}"
+: "${ORGANIZATION_ID:?ORGANIZATION_ID env var must provide numeric organization ID}"
 : "${PROJECT_ID:?PROJECT_ID env var must be provided}"
+: "${PROJECT_NAME:?PROJECT_NAME env var must provide human-readable name of project to create}"
 
-echo "${GCP_CREDS_JSON}" > creds.json
+echo "${GCP_CREDENTIALS_JSON}" > creds.json
 gcloud auth activate-service-account --key-file creds.json
 
 matching_project="$(gcloud projects list --format json | jq '.[] | select(.projectId=="${PROJECT_ID)") | .projectId')"
@@ -33,8 +38,14 @@ fi
 pushd gcp-bootstrap/tf/ci
   terraform init
 
-  TF_VAR_gcp_creds="${GCP_CREDS_JSON}" \
+
+  TF_VAR_billing_account_id="${BILLING_ACCOUNT_ID}" \
+    TF_VAR_bucket_location="${BUCKET_LOCATION}" \
+    TF_VAR_folder_name="${FOLDER_NAME}" \
+    TF_VAR_gcp_creds="${GCP_CREDENTIALS_JSON}" \
+    TF_VAR_organization_id="${ORGANIZATION_ID}" \
     TF_VAR_project_id="${PROJECT_ID}" \
+    TF_VAR_project_name="${PROJECT_NAME}" \
     terraform apply \
     -auto-approve \
     -input=false \
--- tf/ci/bucket.tf
+++ tf/ci/bucket.tf
@@ -1,35 +1,31 @@
+variable "billing_account_id" {}
+variable "bucket_location" {}
+variable "folder_name" {}
 variable "gcp_creds" {}
+variable "organization_id" {}
 variable "project_id" {}
+variable "project_name" {}
 
 provider "google" {
   credentials = "${var.gcp_creds}"
 }
 
-data "google_organization" "org" {
-  domain = "engineerbetter.com"
+data "google_active_folder" "folder" {
+  display_name = "${var.folder_name}"
+  parent       = "organizations/${var.organization_id}"
 }
 
-data "google_billing_account" "acct" {
-  display_name = "EngineerBetter"
-  open         = true
-}
-
-data "google_active_folder" "spikes" {
-  display_name = "spikes"
-  parent       = "organizations/${data.google_organization.org.id}"
-}
-
-resource "google_project" "my_project" {
-  name            = "GCP Spike"
+resource "google_project" "automated_project" {
+  name            = "${var.project_name}"
   project_id      = "${var.project_id}"
-  folder_id       = "${data.google_active_folder.spikes.name}"
-  billing_account = "${data.google_billing_account.acct.id}"
+  folder_id       = "${data.google_active_folder.folder.name}"
+  billing_account = "${var.billing_account_id}"
 }
 
 resource "google_storage_bucket" "ci" {
   name          = "${var.project_id}"
-  location      = "EU"
-  project       = "${google_project.my_project.project_id}"
+  location      = "${var.bucket_location}"
+  project       = "${google_project.automated_project.project_id}"
   force_destroy = true
 
   versioning {

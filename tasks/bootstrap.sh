395350844892e20f7bd8cb166add83c61f68fb48
Daniel Jones <daniel.jones@engineerbetter.com>
2019-05-07 09:11:24 +0100

    make creds available

--- tasks/bootstrap.sh
+++ tasks/bootstrap.sh
@@ -2,7 +2,11 @@
 
 set -euxo pipefail
 
+: "${GCP_CREDS_JSON:?GCP_CREDS_JSON env var must provide contents of credentials file}"
 : "${PROJECT_ID:?PROJECT_ID env var must be provided}"
+
+echo "${GCP_CREDS_JSON}" > creds.json
+export GOOGLE_APPLICATION_CREDENTIALS=$PWD/creds.json
 
 matching_project="$(gcloud projects list --format json | jq '.[] | select(.projectId=="${PROJECT_ID)") | .projectId')"
 
@@ -29,7 +33,9 @@ fi
 pushd gcp-bootstrap/tf/ci
   terraform init
 
-  terraform apply \
+  TF_VAR_gcp_creds="${GCP_CREDS_JSON}" \
+    TF_VAR_project_id="${PROJECT_ID}" \
+    terraform apply \
     -auto-approve \
     -input=false \
     -state=../../../terraform.tfstate \

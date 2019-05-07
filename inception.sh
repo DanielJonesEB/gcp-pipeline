bceb3d51b4c33a77e856812b2a92586e77c3cbeb
Daniel Jones <daniel.jones@engineerbetter.com>
2019-05-07 16:59:59 +0100

    inception script creates folder and user

--- inception.sh
+++ inception.sh
@@ -3,13 +3,19 @@
 set -euxo pipefail
 
 : "${BILLING_ACCOUNT_ID:?BILLING_ACCOUNT_ID env var must specify the ID of the linked billing account}"
-: "${FOLDER_ID:?FOLDER_ID env var must specify which folder to place the inception project in}"
+: "${FOLDER_NAME:?FOLDER_NAME env var must specify which folder to place the inception project in}"
 : "${ORG_ID:?ORG_ID env var must specify the Google Cloud organisation}"
+: "${PROJECT_ID:?PROJECT_ID env var must specify globally unique ID for inception project}"
+
+gcloud alpha resource-manager folders create --display-name="${FOLDER_NAME}" --organization "${ORG_ID}"
+FOLDER_ID="$(gcloud alpha resource-manager folders list --organization 478443594646 --format json | jq -r --arg FOLDER_NAME "spikes" '.[] | select(.displayName==$FOLDER_NAME) | .name | ltrimstr("folders/")')"
 
 gcloud projects create ${PROJECT_ID} --folder ${FOLDER_ID}
 gcloud beta billing projects link ${PROJECT_ID} --billing-account ${BILLING_ACCOUNT_ID}
 
-gcloud iam service-accounts keys create inception-creds.json \
+gcloud iam service-accounts create inception --display-name "Inception User" --project ${PROJECT_ID}
+
+gcloud iam service-accounts keys create ${PROJECT_ID}.json \
   --iam-account inception@${PROJECT_ID}.iam.gserviceaccount.com \
   --project ${PROJECT_ID}
 
